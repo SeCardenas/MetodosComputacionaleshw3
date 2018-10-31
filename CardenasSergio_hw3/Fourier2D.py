436931e39855052b57e81471e7e59ed9697016c7
secardenas <se.cardenas@uniandes.edu.co>
2018-10-31 18:53:45 -0500

    variación de algunos parámetros para mejorar el filtro

--- CardenasSergio_hw3/Fourier2D.py
+++ CardenasSergio_hw3/Fourier2D.py
@@ -8,7 +8,7 @@ image = plt.imread('arbol.png').astype(float)
 imfft = fft2(image, axes=(0,1))
 
 plt.figure()
-plt.imshow(np.abs(imfft), norm=LogNorm(vmin=5))
+plt.imshow(np.abs(imfft), norm=LogNorm(vmin=5)) #Escala LogNorm (con vmin=5 se observa claramente la ubicacion del ruido)
 plt.colorbar()
 plt.title('Transformada de Fourier de la imagen')
 plt.savefig('CardenasSergio_FT2D.pdf')
@@ -19,7 +19,7 @@ plt.close()
 #Esto quiere decir que lo mas probable es que los puntos rojos vistos en la grafica sean el ruido.
 
 #Se define la fraccion de coeficientes que se van a mantener, es decir, que no se van a considerar al aplicar el filtro (se puede variar).
-fraction = 0.1
+fraction = 0.088
 #filas (r) y columnas (c)
 r,c = imfft.shape
 #Copia de la transformada
@@ -31,3 +31,16 @@ imfft2[int(r*fraction):int(r*(1-fraction))] = coef2rm*(abs(coef2rm)<maxvalue) #S
 #Se repite el mismo procedimiento en la otra direccion.
 coef2rm = imfft2[:, int(c*fraction):int(c*(1-fraction))]
 imfft2[:, int(c*fraction):int(c*(1-fraction))] = coef2rm*(abs(coef2rm)<maxvalue)
+
+#Grafica de la transformada modificada
+plt.figure()
+plt.imshow(np.abs(imfft2), norm=LogNorm(vmin=5)) #Escala LogNorm
+plt.colorbar()
+plt.savefig("CardenasSergio_FT2D_filtrada.pdf",bbox_inches="tight")
+plt.close()
+
+#Despues de hacer algunas pruebas variando las variables fraction y maxvalue se concluyo lo siguiente:
+#1000 es un buen valor para la variable maxvalue (al ver la grafica de la transformada se entiende por que se escoge este valor).
+#Si se selecciona fraction = 0.09, hay una componente del ruido que no se esta considerando al filtrar.
+#Al escoger fraction = 0.089, se estan considerando todos los puntos en donde hay ruido y se observa que TODOS los puntos rojos ahora son blancos.
+#Se escoje fraction = 0.088 por si acaso :v
